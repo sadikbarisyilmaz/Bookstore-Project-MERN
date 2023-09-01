4fb469d2411684551f5085e9d6855265653cafcf
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-01 15:15:50 +0300

    Feat: added middleware and a route to save books

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,38 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+Book;
 
 const app = express();
+
+//Middleware for parsing reques body
+app.use(express.json());
 
 app.get("/", (req, res) => {
   //   console.log(req);
   return res.status(234).send("Welcome to MERN");
+});
+
+//Route for Save a new book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
