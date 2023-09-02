f2f351fccc9a33161a65014322a1a1505cd4dab3
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-02 14:11:23 +0300

    Feat added routes to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+import ShowBook from "./pages/ShowBook";
+import Home from "./pages/Home";
 const App = () => {
-  return <div className=" bg-orange-300 ">App</div>;
+  <Routes className=" bg-orange-300 ">
+    <Route path="/" element={<Home />} />
+    <Route path="/books/create" element={<CreateBook />} />
+    <Route path="/books/details/:id" element={<ShowBook />} />
+    <Route path="/books/edit/:id" element={<EditBook />} />
+    <Route path="/books/delete/:id" element={<DeleteBook />} />
+  </Routes>;
 };
 
 export default App;
